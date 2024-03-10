30ea41c160d13ebfb929b5ac19413788dd405554
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2024-03-10 21:55:16 +0900

    학습

--- DoitSwift/Map/Map/ViewController.swift
+++ DoitSwift/Map/Map/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     @IBOutlet var myMap: MKMapView!
     @IBOutlet var lblLocationInfo1: UILabel!
     @IBOutlet var lblLocationInfo2: UILabel!
@@ -27,8 +27,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation() // 위치 업데이트 시작
         myMap.showsUserLocation = true // 위치 보기 값을 true로 설정
     }
-
-    func goLoction(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) {
+    
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) -> CLLocationCoordinate2D {
         // 위도 값과 경도 값을 매개변수로 하여 CLLocation Coordinate2DMake 함수를 호출하고, 리턴 값을 pLcation으로 받습니다.
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         // 범위 값을 매개변수로 하여 CLLocation Coordinate2DMake 함수를 호출하고, 리턴 값을 spanValue로 받습니다
@@ -37,32 +37,70 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
         // pRegion 값을 매개변수로 하여 myMap.setRegion함수를 호출합니다.
         myMap.setRegion(pRegion, animated: true)
+        return pLocation
+    }
+    
+    func setAnnotation(latitudeValue: CLLocationDegrees,
+                       longitudeValue: CLLocationDegrees,
+                       delta span: Double,
+                       title strTitle: String,
+                       subtitle strSubtitle: String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let pLocation = locations.last // 위치가 업데이트되면 먼저 마지막 위치 값을 찾아냅니다.
-        // 마지막 위치의 위도, 경도 값을 가지고 앞에서 만든 goLocation 함수를 호출합니다.
-        // 이때 delta 값은 지도의 크기를 정하는데, 값이 작을수록 확대되는 효과가 있습니다. delta를 0.01로 하였으니 1의 값보다 지도를 100배로 확대해서 보여줍니다.
-        goLoction(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        let pLocation = locations.last
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
         CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
             (placemarks, error) -> Void in
-            let pm = placemarks!.first // placemarks 값의 첫 부분만 pm 상수로 대입합니다.
-            let country = pm!.country // pm 상수에서 나라 값을 country 상수에 대입합니다.
-            var address:String = country! // 문자열 address에 country 상수의 값을 대입합니다.
+            let pm = placemarks!.first
+            let country = pm!.country
+            var address:String = country!
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
             if pm!.thoroughfare != nil {
-                address += " " // pm 상수에서 도로 값이 존재하면 address 문자열에 추가합니다.
+                address += " "
                 address += pm!.thoroughfare!
             }
             
             self.lblLocationInfo1.text = "현재 위치"
             self.lblLocationInfo2.text = address
-        })
+            })
         
-        locationManager.stopUpdatingLocation() // 마지막으로 위치가 업데이트되는 것을 멈추게 합니다
+        locationManager.stopUpdatingLocation()
     }
     
+    
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
-        
+        if sender.selectedSegmentIndex == 0 {
+            // 현재 위치 표시
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManager.startUpdatingLocation()
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            // 폴리텍대학 표시
+            setAnnotation(latitudeValue: 37.751853,
+                          longitudeValue: 128.87605740000004,
+                          delta: 1,
+                          title:"한국폴리텍대학 강릉캠퍼스", subtitle: "강원도 강릉시 남산초교길 121")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "한국폴리텍대학 강릉캠퍼스"
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            // 이지퍼블리싱 표시
+            setAnnotation(latitudeValue: 37.556876,
+                          longitudeValue: 126.914066,
+                          delta: 0.1, title: "이지스퍼블리싱", subtitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "이지스퍼블리싱 출판사"
+        }
     }
     
 }
